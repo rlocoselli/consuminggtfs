c139c3f4fac348fa16423d034ea537afec423055
rlocoselli <rlocoselli@yahoo.com.br>
2021-05-16 21:56:48 +0200

    Adding script to update headsigns for Porto Alegre GTFS

--- SQLite/updateTripHeadSign.sql
+++ SQLite/updateTripHeadSign.sql
@@ -1,6 +1,6 @@
-UPDATE trip set trip_headsign = (select stop_name from STOPS s, STOPSTIME s2 
-where s2.trip_id = trip.trip_id
+UPDATE trip as a set trip_headsign = (select stop_name from STOPS s, STOPSTIME s2 
+where s2.trip_id = a.trip_id
+and   s.stop_id = s2.stop_id
 and   stop_sequence in(select max(stop_sequence) 
                        from STOPSTIME s3 
-                       where s3.stop_id = s2.stop_id 
-                       and   s3.trip_id = s2.trip_id))
+                       where s3.trip_id = s2.trip_id))
